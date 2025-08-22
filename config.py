7ea791c20e1a56563608be67957b6e7a91a45d05
Harshit Bilagi <HarshitBilagi@users.noreply.github.com>
2025-08-22 19:18:13 +0530

    New updates for cloud deployment

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
@@ -9,13 +9,11 @@ EMAIL_USER = os.getenv('EMAIL_USER')
 EMAIL_PASS = os.getenv('EMAIL_PASS')
 RECIPIENT_EMAIL = os.getenv('RECIPIENT_EMAIL')
 
-LEETCODE_SESSION = os.getenv('LEETCODE_SESSION')
-CSRF_TOKEN = os.getenv('CSRF_TOKEN')
+# LEETCODE_SESSION = os.getenv('LEETCODE_SESSION')
+# CSRF_TOKEN = os.getenv('CSRF_TOKEN')
 LEETCODE_USERNAME = os.getenv('LEETCODE_USERNAME')
-
-DATABASE_PATH = 'data/problems.db'
 
 DAILY_PROBLEMS_COUNT = 2 
 DAILY_SEND_TIME = "07:00"
 
-LOG_FILE = 'revision_agent.log'
+# LOG_FILE = 'revision_agent.log'
